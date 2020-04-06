6100e27d9dace992c9f880ec55ce1e6d85b85915
YouriFrancois <yourifrancois97@gmail.com>
2020-04-06 12:15:14 -0400

    add heroku link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -4,6 +4,7 @@ let apiUrl
 const apiUrls = {
   production: 'https://rhubarb-cake-91816.herokuapp.com',
   development: 'http://localhost:4741'
+  //development: 'https://rhubarb-cake-91816.herokuapp.com'
 }
 
 if (window.location.hostname === 'localhost') {
