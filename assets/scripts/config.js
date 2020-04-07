a8e496d848027ee22023dd91f950fe43ab6b43a3
YouriFrancois <yourifrancois97@gmail.com>
2020-04-07 10:32:56 -0400

    if statement in handlebar

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,8 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://rhubarb-cake-91816.herokuapp.com',
-  development: 'http://localhost:4741'
+  production:'https://rhubarb-cake-91816.herokuapp.com',
+  development:'http://localhost:4741'
   //development: 'https://rhubarb-cake-91816.herokuapp.com'
 }
 
